d6c9a50c133e5dec2f9c3d7bd0bd4e801c1b3741
Marcelo (Senshyo) <ct.marcelo@hotmail.com>
2024-02-14 19:17:05 -0300

    correção bug

--- Components/response.py
+++ Components/response.py
@@ -5,11 +5,15 @@ def get_response(user_message):
         return "Você é bem queito x-x"
     elif "oi" in msg:
         return "olá"
-    elif "como você está?" or "como voce esta?" in msg:
+    elif "como você está?" in msg or "como voce esta?" in msg:
         return "Estou bem, obrigado por perguntar!"
     elif "python" in msg:
         return "Python é uma linguagem de programação poderosa e versátil!"
     elif "github" in msg:
         return "GitHub é uma plataforma popular para hospedagem de código-fonte e colaboração em projetos de software!"
+    elif "adeus" in msg:
+        return "Adeus!"
+    elif "lukita" in msg:
+        return "lurguita"
     else:
         return "Desculpe, não entendi. Poderia repetir de outra forma?"
